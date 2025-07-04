b143f914a3a12d3792c55ec8216dafc9efa698c4
aaron <jesuaroncharles@gmail.com>
2025-07-04 12:14:00 +0530

    upt:  I have change the cookieParser before the express

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,10 @@ app.use(cors({
   exposedHeaders: ['Content-Range', 'X-Content-Range']
 }));
 
+app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cookieParser());
+
 
 // Serve uploaded files statically
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
